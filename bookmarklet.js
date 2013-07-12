04e831aabda2f633f4350acc0a3d034a2aafcd6a
chanux <chanuka@thinkcube.com>
2013-07-12 22:30:58 +0530

    Fixes issue in bookmarklet

--- bookmarklet.js
+++ bookmarklet.js
@@ -7,20 +7,18 @@ javascript:
         _key_master.type='text/javascript';
 		_key_master.src='https://raw.github.com/madrobby/keymaster/master/keymaster.min.js';
 		_key_master.onload=_key_master.onreadystatechange = function(){
-			key('ctrl+e', function(){
-                //make keymaster work on input/textarea fields
-                key.filter = function(){return true;};
-				window.toggleSayura();
-			});
+            //make keymaster work on input/textarea fields
+            key.filter = function(){ return true; };
+            key('ctrl+e', function(){ window.toggleSayura(); });
 		};
 
 		document.getElementsByTagName('head')[0].appendChild(_key_master);
-	}
+	};
 
-	if(!document.getElementById("sayurajs")){
+	if(window.toggleSayura === undefined){
 		_sayura_js=document.createElement('script');
 		_sayura_js.type='text/javascript';
 		_sayura_js.src='https://raw.github.com/chanux/sayura.js/master/sayura.js';
 		document.getElementsByTagName('head')[0].appendChild(_sayura_js);
-	}
+	};
 })();
