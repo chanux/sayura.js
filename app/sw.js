860a7605a3d75bddaf847569f6982747d93d7d5d
chanux <chanux@gmail.com>
2020-06-01 23:47:55 +0800

    Initiates PWA

---
+++ app/sw.js
@@ -0,0 +1,29 @@
+const version = "0.3.0";
+const cacheName = `sayura-${version}`;
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        `/`,
+        `/index.html`,
+        `/favicon.ico`,
+        `/sayura.js`
+      ])
+          .then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
